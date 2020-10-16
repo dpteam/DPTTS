5c60c5e3c9b99ff6f5e2697dd1c8b8ae0bc14584
DartPower <SIMPSON-NEKEK@mail.ru>
2020-10-16 13:29:14 +0300

    Addition

--- DPTTS/Program.cs
+++ DPTTS/Program.cs
@@ -1,16 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows;
 
 namespace DPTTS
 {
     class Program
     {
+        [STAThread]
         static void Main(string[] args)
         {
-            
+            Marshal.PrelinkAll(typeof(Program));
+            Trace.AutoFlush = true;
+            Trace.Listeners.Clear();
+            Trace.Listeners.Add(new ConsoleTraceListener());
+            Trace.Listeners.Add(new TextWriterTraceListener(System.IO.Path.GetFileNameWithoutExtension(typeof(Program).Assembly.GetName().Name) + ".log"));
+            Process currentProcess = Process.GetCurrentProcess();
+            currentProcess.PriorityClass = ProcessPriorityClass.High;
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+            try
+            {
+
+            }
+            catch (Exception value)
+            {
+                try
+                {
+                    MessageBox.Show(value.ToString(), "Error");
+                }
+                catch
+                {
+                }
+            }
         }
     }
 }
