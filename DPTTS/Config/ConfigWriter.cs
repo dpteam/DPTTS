808543c3b5b667946d1e9d31c89c6be9e8ab2fec
DartPower <SIMPSON-NEKEK@mail.ru>
2020-10-16 14:30:34 +0300

    Фух, допилил эту часть

--- DPTTS/Config/ConfigWriter.cs
+++ DPTTS/Config/ConfigWriter.cs
@@ -7,10 +7,7 @@ namespace DPTTS.Config
         public static void Config_Write()
         {
             IniFile INI = new IniFile("config.ini");
-            INI.WriteINI("SettingForm1", "Height", numericUpDown2.Value.ToString());
-            INI.WriteINI("SettingForm1", "Width", numericUpDown1.Value.ToString());
-            this.Height = int.Parse(numericUpDown1.Value.ToString());
-            this.Width = int.Parse(numericUpDown2.Value.ToString());
+            INI.Write("DPTTS", "Token", ConfigManager.Token);
             MessageBox.Show("Настройки сохранены", "Информация", MessageBoxButtons.OK, MessageBoxIcon.Asterisk); // Говорим пользователю, что сохранили конфиг.
         }
     }
--- DPTTS/Config/ReadConfig.cs
+++ DPTTS/Config/ReadConfig.cs
@@ -8,15 +8,9 @@ namespace DPTTS.Config
         {
             IniFile INI = new IniFile("config.ini");
             if (INI.KeyExists("DPTTS", "Token"))
-                numericUpDown2.Value = int.Parse(INI.ReadINI("SettingForm1", "Height"));
+                ConfigManager.Token = (string)INI.ReadINI("DPTTS", "Token");
             else
-                numericUpDown1.Value = this.MinimumSize.Height;
-
-            if (INI.KeyExists("SettingForm1", "Width"))
-                textBox1.Text = INI.ReadINI("Other", "Text");
-
-            this.Height = int.Parse(numericUpDown1.Value.ToString());
-            this.Width = int.Parse(numericUpDown2.Value.ToString());
+                ConfigManager.Token = "";
 
             MessageBox.Show("Настройки загружены", "Информация", MessageBoxButtons.OK, MessageBoxIcon.Asterisk); // Говорим пользователю, что загрузили конфиг.
         }
